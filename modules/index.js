d0695a23272eb081ecfd4b4935f634c39c3fec38
AsgherHusaain <asgher.logicchaos@gmail.com>
2024-06-05 02:54:20 +0500

    f1

---
+++ modules/index.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { SafeAreaView, StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+
+const WelcomeScreen = () => {
+  return <SafeAreaView style={styles.container}>
+      <View style={styles.headerContainer}>
+        <Text style={styles.headerText}>Welcome!</Text>
+      </View>
+      <Image source={{
+      uri: 'https://tinyurl.com/42evm3m3'
+    }} style={styles.image} />
+      <View style={styles.textContainer}>
+        <Text style={styles.descriptionText}>
+          Discover a wide range of amazing features tailored just for you.
+        </Text>
+      </View>
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Get Started</Text>
+      </TouchableOpacity>
+    </SafeAreaView>;
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#F5FCFF'
+  },
+  headerContainer: {
+    marginBottom: 20
+  },
+  headerText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#333'
+  },
+  image: {
+    width: 300,
+    height: 300,
+    borderRadius: 150
+  },
+  textContainer: {
+    marginTop: 20,
+    paddingHorizontal: 40
+  },
+  descriptionText: {
+    fontSize: 16,
+    textAlign: 'center',
+    color: '#666'
+  },
+  button: {
+    marginTop: 30,
+    backgroundColor: '#007BFF',
+    paddingVertical: 12,
+    paddingHorizontal: 60,
+    borderRadius: 25,
+    elevation: 3
+  },
+  buttonText: {
+    color: '#FFFFFF',
+    fontSize: 18,
+    fontWeight: 'bold'
+  }
+});
+export default WelcomeScreen;
---
+++ options/options.js
@@ -0,0 +1,13 @@
+export const globalOptions = {
+  name: "borrowed-47114",
+  url: "https://borrowed-47114.botics.co",
+  api: "https://borrowed-47114.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+};
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+};
